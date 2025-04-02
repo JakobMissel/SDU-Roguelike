00288148558f52a79889deb8709a1a09ab581d31
deadlap <llabildgaard@hotmail.com>
2025-04-02 14:57:24 +0200

    Ability stuffs

---
+++ Assets/Scripts/Abilities/Modifier.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Modifier : MonoBehaviour {
+    public enum AbilityModifier {
+        Cooldown,
+        Damage,
+        Range,
+        Area,
+        // CastTime,
+    }
+}
