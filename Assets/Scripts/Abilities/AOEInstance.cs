82447c158389cfbbd97bc471f717dcd0a396f91b
deadlap <32266596+deadlap@users.noreply.github.com>
2025-04-23 10:55:14 +0200

    stuffs

--- Assets/Scripts/Abilities/AOEInstance.cs
+++ Assets/Scripts/Abilities/AOEInstance.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 public class AOEInstance : AbilityInstance {
     List<Health> ObjectsInZone;
     void Start() {
-        ObjectsInZone = new List<Health>();
+        // ObjectsInZone = new List<Health>();
     }
 
     void Update() {
@@ -16,20 +16,20 @@ public class AOEInstance : AbilityInstance {
 
     }
 
-    void OnTriggerEnter(Collider other) {
-        if ((other.gameObject.CompareTag("Enemy") && SourceAbility.PlayerAbility) || 
-        (other.gameObject.CompareTag("Player") && !SourceAbility.PlayerAbility)){
-            other.gameObject.GetComponent<Health>().TakeDamage(SourceAbility.CalculateDamage());
-        }
-        // if (other.gameObject.CompareTag("Player") && !SourceAbility.PlayerAbility){
-        //     other.gameObject.GetComponent<Health>().TakeDamage(SourceAbility.CalculateDamage());
-        //     Destroy(this);
-        // }
-        // switch(other.gameObject.tag){
-        //     case "Enemy":
-        //     break;
-        //     case "Player":
-        //     break;
-        // }
-    }
+    // void OnTriggerEnter(Collider other) {
+    //     if ((other.gameObject.CompareTag("Enemy") && SourceAbility.PlayerAbility) || 
+    //     (other.gameObject.CompareTag("Player") && !SourceAbility.PlayerAbility)){
+    //         other.gameObject.GetComponent<Health>().TakeDamage(SourceAbility.CalculateDamage());
+    //     }
+    //     // if (other.gameObject.CompareTag("Player") && !SourceAbility.PlayerAbility){
+    //     //     other.gameObject.GetComponent<Health>().TakeDamage(SourceAbility.CalculateDamage());
+    //     //     Destroy(this);
+    //     // }
+    //     // switch(other.gameObject.tag){
+    //     //     case "Enemy":
+    //     //     break;
+    //     //     case "Player":
+    //     //     break;
+    //     // }
+    // }
 }
