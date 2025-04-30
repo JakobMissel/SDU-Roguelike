6e6d7f5b62db06813cc91459e4c57992664e46f7
Akaidus <nikolajjensen6854@gmail.com>
2025-04-30 15:02:57 +0200

    Update BreathInstance.cs

--- Assets/Scripts/Abilities/BreathInstance.cs
+++ Assets/Scripts/Abilities/BreathInstance.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 public class BreathInstance : AbilityInstance {
     void Update() {
+        if(SourceAbility == null) return;
         transform.SetPositionAndRotation(SourceAbility.transform.position, Quaternion.LookRotation(SourceAbility.transform.forward));
     }
 }
