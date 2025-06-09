22cc637101e955774fff6dcb5268a6396f2c76ca
Beckudrup <54031477+Beckudrup@users.noreply.github.com>
2025-06-09 10:40:27 +0200

    Short range special Tamer, ambience not spammed

--- Assets/Scripts/DontDestroyHehe.cs
+++ Assets/Scripts/DontDestroyHehe.cs
@@ -2,8 +2,18 @@ using UnityEngine;
 
 public class DontDestroyHehe : MonoBehaviour
 {
+    public static DontDestroyHehe Instance;
     void Awake()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
         DontDestroyOnLoad(gameObject);
+        
     }
 }
